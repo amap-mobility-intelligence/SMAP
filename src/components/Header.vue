1ca586f6e3a1320fc99f2cb71c338e4538f5c3d7
张文捷 <zhangwenjie@zhangwenjiedeMacBook-Air.local>
2026-02-27 03:40:07 +0800

    Header change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,6 +15,22 @@ const logo = {
     link: "#",
 }
 
+// 右侧更多菜单
+const more_paper = {
+    "ExtDM": {
+        name: "ExtDM - CVPR 2024",
+        link: "https://zzcheng.top/ExtDM/",
+    },
+    "MPOT": {
+        name: "MPOT - ICCV 2023",
+        link: "https://zzcheng.top/MPOT/",
+    },
+    "ControlNet": {
+        name: "ControlNet - ICCV 2023",
+        link: "https://github.com/lllyasviel/ControlNet",
+    }
+}
+
 </script>
 
 <template>
@@ -39,21 +55,6 @@ const logo = {
             <el-icon :size="20"><SuccessFilled /></el-icon>
             <a :href=logo.link>{{ logo.name }} </a>
         </el-menu-item>
-
-        <!-- 更多栏目 -->
-        <el-sub-menu index="2">
-            <template #title>More</template>
-            <el-menu-item index="2-1">
-                <a :href=more_paper.ExtDM.link>{{ more_paper.ExtDM.name }}</a>
-            </el-menu-item>
-            <el-menu-item index="2-2">
-                <a :href=more_paper.MPOT.link>{{ more_paper.MPOT.name }}</a>
-            </el-menu-item>
-            <el-menu-item index="2-3">
-                <a :href=more_paper.ControlNet.link>{{ more_paper.ControlNet.name }}</a>
-            </el-menu-item>
-        </el-sub-menu>
-
     </el-menu>
     </el-affix>
 </template>
